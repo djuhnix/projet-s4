c20aad4e49c1e5c88c07833d3d15c9b1ca40e56a
Germain Olea Oyougou <germain.olea-oyougou@etudiant.univ-reims.fr>
2020-03-02 14:29:49 +0100

    Add enum Color and Symbol class

---
+++ src/chess/util/Color.java
@@ -0,0 +1,13 @@
+package chess.util;
+
+/**
+ * Représente les couleurs des pièces du jeu d'échecs.
+ * 
+ * @author olea0001
+ *
+ */
+public enum Color implements java.lang.Comparable<Color>, java.io.Serializable {
+	
+	 BLACK,
+	 WHITE;	
+}
---
+++ src/chess/util/Symbol.java
@@ -0,0 +1,20 @@
+package chess.util;
+
+public class Symbol 
+{
+	public static char 	BLACK_BISHOP 	= '♝';
+	public static char 	BLACK_KING 		= '♚';
+	public static char 	BLACK_KNIGHT 	= '♞';
+	public static char 	BLACK_PAWN 		= '♟';
+	public static char 	BLACK_QUEEN 	= '♛';
+	public static char 	BLACK_ROOK 		= '♜';
+	
+	public static char 	WHITE_BISHOP 	= '♗';
+	public static char 	WHITE_KING 		= '♔';
+	public static char 	WHITE_KNIGHT 	= '♘';
+	public static char 	WHITE_PAWN 		= '♙';
+	public static char 	WHITE_QUEEN 	= '♕';
+	public static char 	WHITE_ROOK 		= '♖';
+	
+	private Symbol() {};
+}
