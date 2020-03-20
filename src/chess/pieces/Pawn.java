6e4df049f064be41d49c1fe37da067cbf81dd375
Germain OLEA-OYOUGOU <djuhngleen@gmail.com>
2020-03-20 19:58:32 +0100

    add Pawn isValidMove method

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -26,10 +26,50 @@ public class Pawn extends Piece {
 
 	@Override
 	public boolean isValidMove(Position destination) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean res = false;
+
+		if ( !this.getPosition().equals(destination) ) {
+
+			// les pions blancs avancent vers le haut
+			if (this.getColor() == Color.WHITE) {
+				if (this.getPosition().getX() < destination.getX()
+						&& this.getPosition().getY() < destination.getY()) {
+					res = true;
+				}
+			} 
+			// les pions noirs avancent vers le bas
+			else {
+				if (this.getPosition().getX() > destination.getX()
+						&& this.getPosition().getY() > destination.getY()) {
+					res = true;
+				}
+			}
+
+			int deltaX = Math.abs(this.getPosition().getX() - destination.getX());
+			int deltaY = Math.abs(this.getPosition().getY() - destination.getY());
+			
+			// Mouvements normaux
+			if ( deltaX  == 1 && this.getPosition().isOnSameColumnAs(destination)) {
+				res = true;
+			}
+			// Premier mouvement
+			else if ( deltaX == 2
+						&& this.getPosition().isOnSameColumnAs(destination)
+						&& this.notMovedYet) {
+				res = true;
+
+			}
+			// Capture : mouvements en diagonales
+			if (!this.notMovedYet
+					&& this.getPosition().isOnSameDiagonalAs(destination)
+					&& ( deltaY == 1 && deltaX == 1)) {
+				res = true;
+			}
+		}
+
+		return res;
 	}
-	
+
 	@Override
 	public void moveTo(Position destination) throws ChessMoveException
 	{
