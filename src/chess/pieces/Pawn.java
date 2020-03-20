e8905f4f18eea0cde846dec1b91d080cfb85df5f
Germain OLEA-OYOUGOU <djuhngleen@gmail.com>
2020-03-20 17:53:52 +0100

    add notMovedYet to Pawn

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -1,11 +1,16 @@
 package chess.pieces;
 
 import chess.Chessboard;
+import chess.util.ChessMoveException;
 import chess.util.Color;
 import chess.util.Position;
 import chess.util.Symbol;
 
 public class Pawn extends Piece {
+	/**
+	 * vrai si le pion n'a pas encore été déplacé. Mis à faux dès le premier déplacement.
+	 */
+	private boolean notMovedYet;
 
 	/**
 	 * Constructeur.
@@ -16,12 +21,20 @@ public class Pawn extends Piece {
 	public Pawn(Chessboard board, Position position, Color color) 
 	{
 		super(board, position, color, "Pawn", color == Color.WHITE ? Symbol.WHITE_PAWN : Symbol.BLACK_PAWN);
+		this.notMovedYet = true;
 	}
 
 	@Override
 	public boolean isValidMove(Position destination) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+	
+	@Override
+	public void moveTo(Position destination) throws ChessMoveException
+	{
+		this.notMovedYet = false;
+		super.moveTo(destination);
 	}
 
 }
